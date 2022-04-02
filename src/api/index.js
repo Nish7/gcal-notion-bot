93700f81f1dcef7f9164b11c66bbb9a3c9b47688
Nishil K <nishilkapadia17@gmail.com>
2022-04-01 23:58:34 -0400

    promisified

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { getEvents } = require('../lib/notion');
-const { insertEvent } = require('../lib/gcal');
+const { getGoogleEvents, insertEvent } = require('../lib/gcal');
 const { getDateTime } = require('../lib/time');
 
 router.get('/events', async (req, res) => {
 	// const events = await getEvents();
+	const exis_events = await getGoogleEvents();
 
-	const events = [
+	const events_to_add = [
 		{
 			title: 'Quiz 1 ',
 			course: 'CPS412',
@@ -21,45 +22,47 @@ router.get('/events', async (req, res) => {
 			status: true,
 			notes: 'WIO: Computer Science as a Catalyst',
 		},
-		{
-			title: 'Final Exam',
-			course: 'MTH207',
-			start_date: '2022-04-22T18:00:00.000-04:00',
-			end_date: '2022-04-24T23:00:00.000-04:00',
-			task: 'Exam',
-			weight: 0,
-			scored: null,
-			weightage: 0.4,
-			status: false,
-		},
-		{
-			title: 'Midterm 1',
-			course: 'MTH207',
-			start_date: '2022-02-11',
-			end_date: '2022-02-13',
-			task: 'Exam',
-			weight: 13.6665,
-			scored: 0.9111,
-			weightage: 0.15,
-			status: true,
-			notes: 'Till Chp 2.5 .. Including Chp1 (No Trig)',
-		},
-		{
-			title: 'Final Exam',
-			course: 'CPS412',
-			start_date: '2022-04-19T15:00:00.000-04:00',
-			end_date: null,
-			task: 'Exam',
-			weight: 0,
-			scored: null,
-			weightage: 0.2,
-			status: false,
-		},
+		// {
+		// 	title: 'Final Exam',
+		// 	course: 'MTH207',
+		// 	start_date: '2022-04-22T18:00:00.000-04:00',
+		// 	end_date: '2022-04-24T23:00:00.000-04:00',
+		// 	task: 'Exam',
+		// 	weight: 0,
+		// 	scored: null,
+		// 	weightage: 0.4,
+		// 	status: false,
+		// },
+		// {
+		// 	title: 'Midterm 1',
+		// 	course: 'MTH207',
+		// 	start_date: '2022-02-11',
+		// 	end_date: '2022-02-13',
+		// 	task: 'Exam',
+		// 	weight: 13.6665,
+		// 	scored: 0.9111,
+		// 	weightage: 0.15,
+		// 	status: true,
+		// 	notes: 'Till Chp 2.5 .. Including Chp1 (No Trig)',
+		// },
+		// {
+		// 	title: 'Final Exam',
+		// 	course: 'CPS412',
+		// 	start_date: '2022-04-19T15:00:00.000-04:00',
+		// 	end_date: null,
+		// 	task: 'Exam',
+		// 	weight: 0,
+		// 	scored: null,
+		// 	weightage: 0.2,
+		// 	status: false,
+		// },
 	];
 
-	events.forEach(
+	const gcal_promises = [];
+
+	events_to_add.forEach(
 		({
-			title: summary,
+			title,
 			course,
 			start_date,
 			end_date,
@@ -74,6 +77,8 @@ router.get('/events', async (req, res) => {
 				end_date
 			);
 
+			const summary = `${course}: ${title}`;
+
 			const evt = {
 				summary,
 				description: `Course: ${course} \n Task: ${task} \n Weight: ${weight} \n Scores: ${scored} \n Weightage: ${weightage} \n Status: ${status} `,
@@ -87,11 +92,30 @@ router.get('/events', async (req, res) => {
 				},
 			};
 
-			insertEvent(evt, allDay, res);
+			const found_events = exis_events.filter(
+				({ summary }) => summary == evt.summary
+			);
+
+			console.log('FoundEvents: ', found_events);
+
+			if (found_events.length > 0) {
+				// dont do anything for now!
+				// update it
+			} else {
+				gcal_promises.push(insertEvent(evt, allDay, res));
+			}
 		}
 	);
 
-	return res.status(201).json({ data: 'Calendar event successfully created.' });
+	Promise.all(gcal_promises)
+		.then(() => {
+			return res
+				.status(201)
+				.json({ data: 'All Calendar events successfully created.' });
+		})
+		.catch((err) => {
+			return res.status(400).json({ error: err });
+		});
 });
 
 
--- src/lib/gcal.js
+++ src/lib/gcal.js
@@ -1,5 +1,4 @@
 const format = require('date-fns/format');
-const addHours = require('date-fns/addHours');
 const { google } = require('googleapis');
 const { OAuth2 } = google.auth;
 
@@ -14,62 +13,84 @@ OAuth2Client.setCredentials({
 
 const calendar = google.calendar({ version: 'v3', auth: OAuth2Client });
 
-function addEvent(evt, allDay, res) {
-	if (allDay) {
-		evt = {
-			...evt,
-			start: {
-				date: format(evt.start.dateTime, 'yyyy-MM-dd'),
-				timeZone: 'Canada/Eastern',
-			},
-			end: {
-				date: format(evt.end.dateTime, 'yyyy-MM-dd'),
-				timeZone: 'Canada/Eastern',
-			},
-		};
-	}
-
-	calendar.events.insert(
-		{
-			calendarId: 'primary',
-			resource: evt,
-		},
-		(err) => {
-			if (err) {
-				console.error(err);
-				return res
-					.status(500)
-					.json({ error: 'Error Creating Calender Event:' + err });
-			}
-		}
-	);
-}
-
 module.exports = {
-	insertEvent: (evt, allDay, res) => {
-		calendar.events.list(
-			{
-				resource: {
-					timeMin: evt.start.dateTime,
-					timeMax: addHours(evt.end.dateTime, 1),
-					items: [
-						{
-							id: 'primary',
-						},
-					],
+	getGoogleEvents: () => {
+		return new Promise((resolve, reject) => {
+			calendar.events.list({ calendarId: 'primary' }, (err, resp) => {
+				if (err) reject(err);
+
+				const items = resp.data.items.map(
+					({ id, start, end, summary, description }) => ({
+						id,
+						start,
+						end,
+						summary,
+						description,
+					})
+				);
+
+				// console.log(items)
+				resolve(items);
+			});
+		});
+	},
+
+	insertEvent: (evt, allDay) => {
+		if (allDay) {
+			evt = {
+				...evt,
+				start: {
+					date: format(evt.start.dateTime, 'yyyy-MM-dd'),
+					timeZone: 'Canada/Eastern',
 				},
-			},
-			(err, resp) => {
-				if (err) return console.error(err);
+				end: {
+					date: format(evt.end.dateTime, 'yyyy-MM-dd'),
+					timeZone: 'Canada/Eastern',
+				},
+			};
+		}
 
-				console.log(resp.data.calendars?.primary);
-
-				if (resp.data.calendars?.primary?.busy.length == 0) {
-					addEvent(evt, allDay, res);
-				} else {
-					return res.status(401).json({ error: `Sorry I'm busy...` });
+		return new Promise((resolve, reject) => {
+			calendar.events.insert(
+				{
+					calendarId: 'primary',
+					resource: evt,
+				},
+				(err) => {
+					if (err) reject(err);
+					resolve();
 				}
-			}
-		);
+			);
+		});
+	},
+
+	updateEvent: (evt, allDay, id) => {
+		if (allDay) {
+			evt = {
+				...evt,
+				start: {
+					date: format(evt.start.dateTime, 'yyyy-MM-dd'),
+					timeZone: 'Canada/Eastern',
+				},
+				end: {
+					date: format(evt.end.dateTime, 'yyyy-MM-dd'),
+					timeZone: 'Canada/Eastern',
+				},
+			};
+		}
+
+		return new Promise((resolve, reject) => {
+			calendar.events.update(
+				{
+					calendarId: 'primary',
+					eventId: id,
+					resource: evt,
+				},
+				(err) => {
+					if (err) reject(err);
+					resolve();
+				}
+			);
+		});
 	},
 };
